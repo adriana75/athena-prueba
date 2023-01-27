e550d616551fe4acb4acba8b999df69a3bd26fd3
adrianaps <tecnologianormal@hotmail.com>
2023-01-27 15:09:27 -0500

    "Configuración general del proyecto"

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express, { Express, Request, Response } from "express"; 
+import dotenv from 'dotenv';
+
+//Configuration the .env file
+dotenv.config();
+
+//Create Express APP
+const app: Express = express();
+const port: string | number = process.env.PORT || 8000
+
+//Define the first Route of APP
+app.get('/', (req: Request, res: Response) =>{
+
+    //Send Hello Word
+    res.send('Welcome APP API Restful: Express + TS + Swagger + Mongoose')
+})
+
+app.get('/hello', (req: Request, res: Response) =>{
+
+    //Send Hello Word
+    res.send('Get Hello')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Express server. Running at http://localhost:${port}`)
+})
